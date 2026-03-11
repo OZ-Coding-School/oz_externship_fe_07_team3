ffd2cc994bbcb7cafd05c327c6c530831243aa31
Clacki <skthk6613@naver.com>
2026-03-11 16:53:30 +0900

    feat: 헤더 푸터 구현 (#31)

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router'
+import Logo from '@/assets/images/logo.png'
+
+function Header() {
+  return (
+    <header className="border-b border-gray-200">
+      <section className="bg-ui-gray-800 text-grey-1 flex h-12 w-full items-center justify-center text-base">
+        🚨 선착순 모집! 국비지원 받고 4주 완성
+      </section>
+
+      <section className="text-ui-gray-600 flex h-16 w-full items-center justify-between px-90 text-[18px] whitespace-nowrap">
+        <div className="flex items-center gap-15">
+          <h1 className="shrink-0">
+            <img
+              className="h-auto w-[120px] object-contain"
+              src={Logo}
+              alt="logo"
+            />
+          </h1>
+
+          <nav aria-label="주요 메뉴">
+            <ul className="flex gap-15">
+              <li className="py-4">
+                <Link to="/community">커뮤니티</Link>
+              </li>
+              <li className="py-4">
+                <Link to="/qna">질의응답</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <nav aria-label="사용자 메뉴">
+          <ul className="flex items-center gap-2">
+            <li className="py-4">
+              <Link to="/login">로그인</Link>
+            </li>
+            <li className="text-xl">|</li>
+            <li className="py-4">
+              <Link to="/signup">회원가입</Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </header>
+  )
+}
+
+export default Header
