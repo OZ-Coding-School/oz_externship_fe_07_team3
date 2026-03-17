3071dd1218be6a8ac9d03d64c7ab5673d140396e
Clacki <skthk6613@naver.com>
2026-03-17 18:56:19 +0900

    feat: 마이페이지 쪽지시험탭 ui 구현 (#65)

--- src/features/mypage/MyExamTab.tsx
+++ src/features/mypage/MyExamTab.tsx
@@ -1,11 +1,123 @@
+import { useState } from 'react'
+import { EXAM_SUBJECT_ICON_MAP } from '@/constants/examSubjectIconMap'
+import type { ExamTabType } from '@/types/mypage-type/examDeployment'
+import { mockExamDeploymentList } from '@/mocks/data/mockExamDeploymentList'
+import {
+  EXAM_STATUS_META,
+  EXAM_TAB_LABEL,
+  EXAM_TABS,
+} from '@/constants/examTab'
+import { Link } from 'react-router-dom'
+
 export default function MyExamTab() {
+  const [activeTab, setActiveTab] = useState<ExamTabType>('all')
+  const filteredExamList = mockExamDeploymentList.results.filter((item) => {
+    if (activeTab === 'all') {
+      return true
+    }
+
+    return item.exam_info.status === activeTab
+  })
   return (
-    <section className="border-grey-3 bg-grey-1 w-186 rounded-lg px-6 py-6">
-      <h1 className="text-grey-6 mb-6 text-[32px] font-bold">쪽지시험</h1>
-      <div className="border-grey-3 flex h-75 items-center justify-center rounded-lg border">
-        <p className="text-grey-5 text-sm">
-          쪽지시험 컴포넌트가 들어올 영역입니다.
-        </p>
+    <section className="w-186">
+      {/* 제목 */}
+      <h1 className="text-primary mb-15 text-[32px] leading-[140%] font-bold tracking-[-0.03em]">
+        쪽지시험
+      </h1>
+
+      {/* 상단 탭 */}
+      <div className="border-grey-9 border-b">
+        <nav aria-label="쪽지시험 상태 탭">
+          <ul className="flex gap-[30px]">
+            {EXAM_TABS.map((tab) => {
+              const isActive = activeTab === tab
+
+              return (
+                <li
+                  key={tab}
+                  className="text-[20px] leading-[140%] font-bold tracking-[-0.03em]"
+                >
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setActiveTab(tab)
+                    }}
+                    aria-pressed={isActive}
+                    className={`border-b-2 p-4 ${
+                      isActive
+                        ? 'border-[#721AE3] text-[#721AE3]'
+                        : 'border-transparent text-gray-400 hover:text-[#721AE3]'
+                    }`}
+                  >
+                    {EXAM_TAB_LABEL[tab]}
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </div>
+
+      {/* 콘텐츠(시험과목) 영역 */}
+      <div className="mt-6">
+        <ul className="flex flex-col gap-4">
+          {filteredExamList.map((item) => {
+            // 과목명에 맞는 아이콘과 시험 상태별 UI 메타 정보를 가져옴, ts검증
+            const subjectTitle = item.exam.subject
+              .title as keyof typeof EXAM_SUBJECT_ICON_MAP
+            const subjectIcon = EXAM_SUBJECT_ICON_MAP[subjectTitle]
+            const statusMeta = EXAM_STATUS_META[item.exam_info.status]
+            const isDone = item.exam_info.status === 'done'
+            // 아직 api data로 이동 X
+            const buttonPath = isDone
+              ? `/quiz/result/${item.id}`
+              : `/quiz/${item.id}`
+            return (
+              <li
+                key={item.id}
+                className="flex items-center gap-4 rounded-lg border px-8 py-7"
+              >
+                <img src={subjectIcon} alt="" className="h-12 w-12" />
+
+                <div className="flex-1">
+                  <div className="flex items-center gap-3">
+                    <h3 className="text-[18px] leading-[140%] font-semibold tracking-[-0.03em]">
+                      {item.exam.title}
+                    </h3>
+                    <p
+                      className={`flex items-center justify-center rounded-[2px] px-2 py-1 text-[12px] ${statusMeta.badgeClassName}`}
+                    >
+                      {statusMeta.label}
+                    </p>
+                  </div>
+
+                  <div className="text-[14px] leading-[140%] font-normal tracking-[-0.03em] text-gray-700">
+                    {isDone ? (
+                      <p>
+                        {item.exam.subject.title} ㆍ {item.exam_info.score}점/
+                        {item.total_score}점 ㆍ{' '}
+                        {item.exam_info.correct_answer_count}/
+                        {item.question_count}개 정답
+                      </p>
+                    ) : (
+                      <p>
+                        {item.exam.subject.title} ㆍ 응시하고 점수를
+                        확인해보세요!
+                      </p>
+                    )}
+                  </div>
+                </div>
+                {/* 버튼영역 */}
+                <Link
+                  to={buttonPath}
+                  className={`${statusMeta.buttonClassName} shrink-0`}
+                >
+                  {statusMeta.buttonText}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </section>
   )
---
+++ src/types/mypage-type/examDeployment.ts
@@ -0,0 +1,33 @@
+export type ExamTabType = 'all' | 'done' | 'pending'
+
+type ExamListStatus = 'done' | 'pending'
+
+export type ExamDeploymentItem = {
+  id: number
+  submission_id: number | null
+  exam: {
+    id: number
+    title: string
+    thumbnail_img_url: string
+    subject: {
+      id: number
+      title: string
+      thumbnail_img_url: string | null
+    }
+  }
+  question_count: number
+  total_score: number
+  exam_info: {
+    status: ExamListStatus
+    score: number | null
+    correct_answer_count: number | null
+  }
+  is_done: boolean
+  duration_time: number
+}
+
+export type ExamDeploymentListResponse = {
+  page: number
+  has_next: boolean
+  results: ExamDeploymentItem[]
+}
