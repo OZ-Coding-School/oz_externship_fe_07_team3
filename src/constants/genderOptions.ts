2a66cb1409d0f22a1a20ee3b622d7837f0957d9b
jwb <jeonwb622@gmail.com>
2026-03-23 17:44:10 +0900

    refactor: 성별 선택 코드 리팩토링 (#103)
    - 성별 선택 공통으로 코드 리팩토링 진행
    - 성별 옵션 상수화 분리

---
+++ src/constants/genderOptions.ts
@@ -0,0 +1,13 @@
+export const GENDER_OPTIONS = [
+  {
+    value: 'M' as const,
+    label: '남',
+    activeClassName:
+      'border-primary-default bg-primary-100 text-primary-default',
+  },
+  {
+    value: 'F' as const,
+    label: '여',
+    activeClassName: 'border-violet-500 bg-violet-50 text-violet-600',
+  },
+]
