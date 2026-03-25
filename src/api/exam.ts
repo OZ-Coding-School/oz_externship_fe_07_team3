73dc73496a97de7e98599ca2563e0fb661322a8d
Clacki <skthk6613@naver.com>
2026-03-25 09:58:07 +0900

    refactor: 경로설정 수정 (#99)

--- src/api/exam.ts
+++ src/api/exam.ts
@@ -1,31 +1,33 @@
+import { EXAM_API_PATHS } from '@/constants/apisPaths'
 import type { SubmitExamResponse } from '@/types/api-response/examResponse'
 import type { ExamDeploymentListResponse } from '@/types/mypage-type/examDeployment'
 import type { QuizData } from '@/types/quizpage-type/question'
 import type { ExamStatusResponse } from '@/types/quizpage-type/status'
-import type { ResultData } from '@/types/result-type/answer'
-
 import type { SubmitExamRequest } from '@/types/quizpage-type/submit'
+import type { ResultData } from '@/types/result-type/answer'
 import { api } from './api'
 
 export const getExamDeployments =
   async (): Promise<ExamDeploymentListResponse> => {
-    const { data } =
-      await api.get<ExamDeploymentListResponse>('/exams/deployments')
+    const { data } = await api.get<ExamDeploymentListResponse>(
+      EXAM_API_PATHS.DEPLOYMENTS
+    )
     return data
   }
 
 export const getExamQuestions = async (
   deploymentId: number
 ): Promise<QuizData> => {
-  const { data } = await api.get<QuizData>(`/exams/deployments/${deploymentId}`)
+  const { data } = await api.get<QuizData>(
+    EXAM_API_PATHS.deploymentDetail(deploymentId)
+  )
   return data
 }
 
 export const checkExamCode = async (deploymentId: number, code: string) => {
-  const { data } = await api.post(
-    `/exams/deployments/${deploymentId}/check-code`,
-    { code }
-  )
+  const { data } = await api.post(EXAM_API_PATHS.checkCode(deploymentId), {
+    code,
+  })
   return data
 }
 
@@ -33,7 +35,7 @@ export const getExamStatus = async (
   deploymentId: number
 ): Promise<ExamStatusResponse> => {
   const { data } = await api.get<ExamStatusResponse>(
-    `/exams/deployments/${deploymentId}/status`
+    EXAM_API_PATHS.status(deploymentId)
   )
   return data
 }
@@ -43,10 +45,9 @@ export const submitExam = async (
   payload: SubmitExamRequest
 ): Promise<SubmitExamResponse> => {
   const { data } = await api.post<SubmitExamResponse>(
-    `/exams/deployments/${deploymentId}/submit`,
+    EXAM_API_PATHS.submit(deploymentId),
     payload
   )
-
   return data
 }
 
@@ -54,7 +55,7 @@ export const getExamResult = async (
   submissionId: number
 ): Promise<ResultData> => {
   const { data } = await api.get<ResultData>(
-    `/exams/submissions/${submissionId}`
+    EXAM_API_PATHS.result(submissionId)
   )
   return data
 }
