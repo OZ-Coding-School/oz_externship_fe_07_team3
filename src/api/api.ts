e7b53dddc32bac70faa422ddb45b7fbc62edda7f
Clacki <skthk6613@naver.com>
2026-03-26 12:59:15 +0900

    refactor: api 수정 (#111)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,8 @@
 import { API_BASE_URL } from '@/constants/apisPaths'
 import axios from 'axios'
 
+import { useAuthStore } from '@/store/authStore'
+
 /**
  * axios - create
  * TODO: baseURL 수정 예정
@@ -15,7 +17,7 @@ export const api = axios.create({
 
 //요청
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('accessToken')
+  const token = useAuthStore.getState().accessToken
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
